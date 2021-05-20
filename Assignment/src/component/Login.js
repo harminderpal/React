b7e816289d868eb36e42d78dc5a4996186ddaedc
Harminder Pal Deep Kaur <71400509+harminderpal@users.noreply.github.com>
2021-05-21 01:51:01 +0530

    Add files via upload

---
+++ Assignment/src/component/Login.js
@@ -0,0 +1,63 @@
+import React, {useState} from "react";
+import  ReactDOM from "react-dom";
+
+class MyApp extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      UserName:"",
+      Password: ""
+    }
+  }
+  // const [UserName, setUserName] = useState("");
+
+  // const [Password, setPassword] = useState("");
+   inputEvent = (eve) => {
+    this.setState({
+      UserName: eve.target.value,
+    });
+   }
+  
+   inputEvent2 = (eve) => {
+    this.setState({
+      Password: eve.target.value,
+    });
+  }
+   onSubmits =(eve) =>{
+    eve.preventDefault();
+    console.log (this.sate.UserName);
+    console.log (this.sate.Password);
+  }
+
+
+render()
+ {
+  return (
+  <><div className= "main_div">
+    <form onSubmit = {this.onSubmits}>
+      <div>
+        <h1>My First Login Page </h1>
+          <input 
+          type ="text"
+          placeholder="Enter Your UserName:"
+          onChange={this.inputEvent}
+          value={this.state.username}
+          />
+          <br/>
+          <input 
+          type ="text"
+          placeholder="Enter Password:"
+          onChange={this.inputEvent2}
+          value={this.state.Password}
+          />
+          <button type ="submit"> submit</button>
+      </div>
+    </form>
+    </div>
+    
+    </>
+);
+}
+
+}
+export default MyApp
